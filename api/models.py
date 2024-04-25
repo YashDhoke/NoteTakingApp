9176812c2cc0053ca92f94c022a45cc0b51bb10f
YashDhoke <yashdhoke1@gmail.com>
2024-04-26 00:22:07 +0530

    created the backend api's

--- api/models.py
+++ api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Note(models.Model):
     title = models.TextField(null=True, blank=True)
-    details = models.TextField(null=True, blank=True)
+    content = models.TextField(null=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
--- api/views.py
+++ api/views.py
@@ -1,3 +1,66 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.filters import OrderingFilter, SearchFilter
+from .models import Note
+from .serializers import NoteSerializer
+from rest_framework import status, generics
+from django.http import Http404
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
 
-# Create your views here.
+
+class  NotesListView(generics.ListAPIView):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
+    ordering_fields = '__all__'
+    ordering = ['-created_at']
+    filterset_fields = '__all__'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset
+    
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
+
+
+class NotesDetailView(APIView):
+    model = Note
+    serializer_class = NoteSerializer
+
+    def get_object(self, pk):
+        try:
+            return self.model.objects.get(pk=pk)
+        except self.model.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        note = self.get_object(pk)
+        serializer = self.serializer_class(note)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        note = self.get_object(pk)
+        serializer = self.serializer_class(note, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        note = self.get_object(pk)
+        note.delete()
+        return Response({'message': 'note deleted'}, status=status.HTTP_204_NO_CONTENT)
+
+
